b72237319da408240d61a482975ec03d4b477ab5
Tesolite <pavel.kotka@gmail.com>
2025-07-23 01:41:34 +0100

    Initial form added to email page

--- src/pages/EmailPage.tsx
+++ src/pages/EmailPage.tsx
@@ -6,7 +6,36 @@ const EmailPage = () => {
   return (
     <PageStyling>
       <HeaderComponent />
-      <PageSection>This is the email page!</PageSection>
+      <PageSection>
+        <form action="" method="post" className="grid grid-cols-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Your Name"
+            className="m-2 border-2 p-2"
+            required
+          />
+          <input
+            type="email"
+            name=""
+            id=""
+            placeholder="Your email"
+            className="m-2 border-2 p-2"
+            required
+          />
+          <textarea
+            name=""
+            id=""
+            placeholder="Your message!"
+            className="col-span-2 m-2 h-[60%] border-2"
+            required
+          ></textarea>
+          <button type="submit" className="col-span-2 m-2 border-2 p-2">
+            Send message
+          </button>
+        </form>
+      </PageSection>
     </PageStyling>
   );
 };
