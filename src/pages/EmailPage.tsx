70955837397075986f4814e14b6725e5492d2ca6
Tesolite <pavel.kotka@gmail.com>
2025-07-25 03:53:19 +0100

    Email form improved styling and colour-coded feedback

--- src/pages/EmailPage.tsx
+++ src/pages/EmailPage.tsx
@@ -10,35 +10,45 @@ const EmailPage = () => {
         <form
           action="https://formsubmit.co/c7bdcf0ca4e623be4deb248dbaa6c530"
           method="POST"
-          className="grid grid-cols-2"
+          className="flex flex-col"
         >
-          <p className="col-span-2 m-2 p-2">
-            Emailing:{" "}
-            {
-              "\u0070\u0061\u0076\u0065\u006c\u002e\u006b\u006f\u0074\u006b\u0061\u0040\u0067\u006d\u0061\u0069\u006c\u002e\u0063\u006f\u006d"
-            }
-          </p>
+          <p className="px-2 text-2xl font-bold">Email Submission</p>
+          <label htmlFor="sender-name" className="mt-2 px-2 text-lg">
+            Name
+          </label>
           <input
             type="text"
+            id="sender-name"
             name="sender-name"
-            placeholder="Your Name"
-            className="m-2 border-2 p-2"
+            placeholder="What should I call you?"
+            className="peer m-2 rounded-md border-1 p-2 transition-all outline-none valid:border-green-500 user-invalid:border-red-500 focus:invalid:border-amber-500"
             required
           />
+          <label htmlFor="sender-email" className="mt-2 px-2 text-lg">
+            Email
+          </label>
           <input
             type="email"
+            id="sender-email"
             name="sender-email"
-            placeholder="Your email"
-            className="m-2 border-2 p-2"
+            placeholder="Best email to reach you?"
+            className="peer m-2 rounded-md border-1 p-2 transition-all outline-none valid:border-green-500 user-invalid:border-red-500 not-placeholder-shown:user-invalid:text-red-500 focus:invalid:border-amber-500 focus:invalid:text-white"
             required
           />
+          <label htmlFor="sender-message" className="mt-2 px-2 text-lg">
+            Message
+          </label>
           <textarea
+            id="sender-message"
             name="sender-message"
-            placeholder="Your message!"
-            className="col-span-2 m-2 h-[60%] border-2"
+            placeholder="Get in touch!"
+            className="peer m-2 h-[60%] rounded-md border-1 p-2 transition-all outline-none valid:border-green-500 user-invalid:border-red-500 focus:invalid:border-amber-500"
             required
           ></textarea>
-          <button type="submit" className="col-span-2 m-2 border-2 p-2">
+          <button
+            type="submit"
+            className="peer-valid m-2 w-fit cursor-pointer self-end rounded-xl border-2 p-2 px-4 font-bold transition-transform hover:-translate-y-0.5 active:translate-y-1"
+          >
             Send message
           </button>
         </form>
